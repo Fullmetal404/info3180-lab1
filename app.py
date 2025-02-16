cc51ad4b4552ffef3c18fea014ccc9694fda8276
Fullmetal404 <gardnerj874@gmail.com>
2025-02-16 16:13:02 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -12,3 +12,11 @@ app = Flask(__name__)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+@app.route('/')
+def home():
+    return 'My home page'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
